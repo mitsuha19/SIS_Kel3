21e93cf3fbb6cfe55b6478a68d92393d27105646
okdinihutagalung <okdinihutagalung@gmail.com>
2024-11-22 20:17:36 +0700

    add dummy data in catatan perilaku page and informasi asrama page

--- resources/views/asrama/asrama.blade.php
+++ resources/views/asrama/asrama.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <!-- Header -->
-    <div class="d-flex align-items-center mb-4 border-bottom-line">
+    <div class="d-flex align-items-center mb-4 border-bottom">
         <h3 class="me-auto">Home / Asrama</h3>
         <a href="{{ route('logout') }}">
             <i class="fas fa-sign-out-alt fs-5 cursor-pointer" title="Logout"></i>
@@ -10,9 +10,56 @@
     </div>
 
     <!-- Konten Utama -->
-    <div class="app-content-header">
-        <div class="container-fluid">
-            <h3 class="mb-0">Asrama </h3>
+    <div class="container mt-4">
+        <h3 class="text-center mb-4">Informasi Asrama</h3>
+        <hr>
+
+        <!-- Informasi Asrama -->
+        <div class="row">
+            <!-- Kolom Kiri -->
+            <div class="col-md-6">
+                <div class="equal-height">
+                    <table class="table table-bordered">
+                        <tbody>
+                            <tr>
+                                <th>ASRAMA</th>
+                                <td>Nazareth, Luar Kampus/Pintubosi</td>
+                            </tr>
+                            <tr>
+                                <th>PENGURUS ASRAMA</th>
+                                <td>
+                                    - Pdt. Irinto Sitorus, S.Th. <br>
+                                    - Pdt. Begawan Johannes Sitompul, S.Th. <br>
+                                    - Pdt. Herman Manurung, S.Th.
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
+            <!-- Kolom Kanan -->
+            <div class="col-md-6">
+                <div class="equal-height">
+                    <table class="table table-bordered">
+                        <tbody>
+                            <tr>
+                                <th>Kamar</th>
+                                <td>Kamar, 1 Lantai 4</td>
+                            </tr>
+                            <tr>
+                                <th>Teman Sekamar</th>
+                                <td>
+                                    - Christian Theofani Napitupulu (2022), S1 Informatika <br>
+                                    - Yisrael Schwartz Sijabat (2022), S1 Informatika <br>
+                                    - Wesly Beretta Siahaan (2022), S1 Informatika <br>
+                                    - Herrmon Rantua Sihombing (2022), S1 Informatika
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
--- resources/views/catatanPerilaku/catatan_perilaku.blade.php
+++ resources/views/catatanPerilaku/catatan_perilaku.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <!-- Header -->
-    <div class="d-flex align-items-center mb-4 border-bottom-line">
+    <div class="d-flex align-items-center mb-4 border-bottom">
         <h3 class="me-auto">Home / Catatan Perilaku</h3>
         <a href="{{ route('logout') }}">
             <i class="fas fa-sign-out-alt fs-5 cursor-pointer" title="Logout"></i>
@@ -10,9 +10,88 @@
     </div>
 
     <!-- Konten Utama -->
-    <div class="app-content-header">
-        <div class="container-fluid">
-            <h3 class="mb-0">Catatan Perilaku </h3>
-        </div>
+    <div class="container mt-4">
+        <h3 class="text-center mb-4">Daftar Nilai Perilaku Mahasiswa</h3>
+        <hr class="mb-4">
+
+        <!-- Tabel -->
+        <table class="table table-bordered table-striped">
+            <thead>
+                <tr>
+                    <th>No</th>
+                    <th>TA</th>
+                    <th>Semester</th>
+                    <th>Skor Awal</th>
+                    <th>Akumulasi Skor</th>
+                    <th>Nilai Huruf</th>
+                    <th></th> <!-- Untuk tombol view di akhir tabel -->
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>2024/2025</td>
+                    <td>Gasal</td>
+                    <td>0</td>
+                    <td>0</td>
+                    <td>A</td>
+                    <td><button class="btn btn-outline-secondary btn-sm"><i class="fas fa-arrow-left"></i></button></td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>2023/2024</td>
+                    <td>Pendek</td>
+                    <td>0</td>
+                    <td>0</td>
+                    <td>A</td>
+                    <td><button class="btn btn-outline-secondary btn-sm"><i class="fas fa-arrow-left"></i></button></td>
+                </tr>
+                <tr>
+                    <td>3</td>
+                    <td>2023/2024</td>
+                    <td>Genap</td>
+                    <td>0</td>
+                    <td>0</td>
+                    <td>A</td>
+                    <td><button class="btn btn-outline-secondary btn-sm"><i class="fas fa-arrow-left"></i></button></td>
+                </tr>
+                <tr>
+                    <td>4</td>
+                    <td>2023/2024</td>
+                    <td>Gasal</td>
+                    <td>0</td>
+                    <td>3</td>
+                    <td>AB</td>
+                    <td><button class="btn btn-outline-secondary btn-sm"><i class="fas fa-arrow-left"></i></button></td>
+                </tr>
+                <tr>
+                    <td>5</td>
+                    <td>2022/2023</td>
+                    <td>Pendek</td>
+                    <td>0</td>
+                    <td>0</td>
+                    <td>A</td>
+                    <td><button class="btn btn-outline-secondary btn-sm"><i class="fas fa-arrow-left"></i></button></td>
+                </tr>
+                <tr>
+                    <td>6</td>
+                    <td>2022/2023</td>
+                    <td>Genap</td>
+                    <td>0</td>
+                    <td>6</td>
+                    <td>B</td>
+                    <td><button class="btn btn-outline-secondary btn-sm"><i class="fas fa-arrow-left"></i></button></td>
+                </tr>
+                <tr>
+                    <td>7</td>
+                    <td>2022/2023</td>
+                    <td>Gasal</td>
+                    <td>0</td>
+                    <td>8</td>
+                    <td>B</td>
+                    <td><button class="btn btn-outline-secondary btn-sm"><i class="fas fa-arrow-left"></i></button></td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 @endsection
