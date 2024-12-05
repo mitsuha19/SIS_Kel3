a6a22f51a6569d38902dcf2fdef9fae2f500b55b
glensofian <glenpardede76@gmail.com>
2024-12-05 15:37:50 +0700

    Proggress in Prodi

--- resources/views/perkuliahan/prodi.blade.php
+++ resources/views/perkuliahan/prodi.blade.php
@@ -13,34 +13,30 @@
         </a>
     </div>
 
-    <!-- Konten Utama -->
-    <div class="app-content-header">
-        <div class="container-fluid">
-            <h3 class="mb-0">Prodi</h3>
-        </div>
-    </div>
-
     <div class="container mt-4">
         <!-- Tampilkan nama program studi -->
         @if ($program)
-            <h4 class="text-center mb-4">{{ $program->nama }}</h4>
-            <hr>
+            <h4 class="h4-Prodi mb-4">{{ $program->nama }}</h4>
+            
 
             <!-- Tampilkan Intro -->
-            <div class="mb-4">
-                <h5>Intro</h5>
-                <p>{{ $program->intro }}</p>
+            <div class="text-left-custom mb-4">
+                <strong>{{ $program->intro }}</strong>
             </div>
 
             <!-- Tampilkan Visi -->
-            <div class="mb-4">
-                <h5>Visi</h5>
+            <div class="text-left-custom mb-4">
+                <div class="underline">
+                    <h5>Visi :</h5>
+                </div>
                 <p>{{ $program->visi }}</p>
             </div>
 
             <!-- Tampilkan Misi -->
-            <div class="mb-4">
-                <h5>Misi</h5>
+            <div class="text-left-custom mb-4">
+                <div class="underline">
+                    <h5>Misi :</h5>
+                </div>
                 <p>{!! nl2br(e($program->misi)) !!}</p>
             </div>
         @else
